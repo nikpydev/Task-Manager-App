4d2e8e1f6cb20df5de4442427a17b12de67a2a97
Nikhil Choudhary <nc1336282@gmail.com>
2019-08-30 00:17:10 +0530

    30-Aug-2019 end of day commit

--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET Requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+// app.use((req, res, next) => {
+//     res.status(503).send('Site is in maintenance mode please try again soon!')
+// })
+
 app.use(express.json()) //parses anything being sent from the client side via the http and converts it into json
 app.use(userRouter)
 app.use(taskRouter)
