0bfa9224083061f141f402e02d1aaeb338b6dbf4
Nikhil Choudhary <nc1336282@gmail.com>
2019-08-31 21:37:03 +0530

    31082019 evening Commit

--- src/index.js
+++ src/index.js
@@ -25,3 +25,18 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
+
+const Task = require('./models/task')
+const User = require('./models/user')
+
+const main = async function() {
+    // const task = await Task.findById('5d6a7d241daef5476c7a8bb2')
+    // await task.populate('owner').execPopulate()
+    // console.log(task.owner);
+
+    const user = await User.findById('5d6a7bd725f2b03aacb32ecb')
+    await user.populate('tasks').execPopulate()
+    console.log(user.tasks);
+}
+
+main()
